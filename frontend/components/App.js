387147e5ec6b0f6bc7b8620736ddf4e04499eae0
Abas Kamara <kamaraabas@aol.com>
2023-02-16 12:11:58 -0500

    stuck on hide completed and show completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -10,7 +10,8 @@ export default class App extends React.Component {
     todos: [],
     error: '',
     todoNameInput:'',
-  }
+    displayCompleted: true,
+    }
 
 onTodoNameInputChange = evt => {
   const {value} = evt.target
@@ -62,6 +63,10 @@ toggleCompleted = id => () => {
     .catch(this.setAxiosResponseError)
 }
 
+toggleDisplayCompleted = () => {
+  this.setState({...this.state, displayCompleted: !this.state.displayCompleted})
+}
+
 componentDidMount() {
   this.fetchAllTodos()
 }
@@ -73,17 +78,23 @@ componentDidMount() {
       <div id='todos'>
         <h2>Todos:</h2>
         {
-          this.state.todos.map(td => {
-            return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? '' : ' ✔️'}</div>
-          })
+          this.state.todos.reduce((acc, td) => {
+            if (this.state.displayCompleted || !td.Completed) {
+              return acc.concat(<div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? '' : ' ✔️'}</div>)
+            }else{
+              return acc
+            }
+          },[])
+            //return 
+          
         }
       </div>
 
       <form id='todoForm' onSubmit={this.onToDoFormSubmit}>
         <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type='text' placeholder="type todo"></input>
         <input type='submit'></input>
-        <button>Clear Completed</button>
       </form>
+        <button onClick={this.toggleDisplayCompleted}>{this.state.displayCompleted ? 'Hide' : 'Show'} Completed</button>
       </div>
     )
   }
